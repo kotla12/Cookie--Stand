48e96592beef0298e4c3c32d3c65753596f09993
kotla12 <kotla9119@gmail.com>
2017-08-14 14:28:07 -0700

    made variables for 1st and Pike store

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 'use strict';
+var openHours = ['6:00 a.m','7:00 a.m','8:00 a.m','9:00 a.m','10:00 a.m','11:00 a.m','12:00 p.m','1:00 p.m','2:00 p.m','3:00 p.m','4:00 p.m','5:00 p.m','6:00 p.m','7:00 p.m','8:00 p.m',];
+var pike = {
+  name : '1st and Pike',
+  minCust : 23,
+  maxCust : 65,
+  AveCookie: 6.3,
+  totalCookies : 0,
+  AveCustHourly : [],
+  AveCookieHourly : []
+} ;
