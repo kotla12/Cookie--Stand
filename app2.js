43442ab4e6b6a2f00e59ee5aebc3f89d61704520
kotla12 <kotla9119@gmail.com>
2017-08-15 16:54:59 -0700

    creating first row of the table with name of store '1st and Pike'

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 'use strict';
 var openHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
-function store (name,minCust, maxCust, aveCookie ) {
+function Store (name,minCust, maxCust, aveCookie ) {
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
@@ -17,28 +17,30 @@ function store (name,minCust, maxCust, aveCookie ) {
       this.totalCookies += this.aveCookieHourly[i];
 
     };
-  };
-  this.show = function() {
-    for (var i = 0; i < this.aveCookieHourly.length; i++) {
-      var node = document.createElement('li');
-      var textnode = document.createTextNode(openHours[i] + ': ' + this.aveCookieHourly[i] + ' Cookies');
-      node.appendChild(textnode);
-      document.getElementById('pike').appendChild(node);
-    }
 
-    var dailyTotal = document.createElement('li');
-    dailyTotal.textContent = 'Total: ' + this.totalCookies + ' Cookies';
-    document.getElementById('pike').appendChild(dailyTotal);
+  };
+  this.showInfo = function(){
+    this.cookieSale ();
+
+    var row = document.createElement('tr');
+    var storeName = document.createElement('th');
+    storeName.textContent = this.name;
+    row.appendChild(storeName);
+
+
   };
 }
+// Objects for each store.
+var pike = new Store('1st and Pike', 23, 65, 6.3);
+var seaTac = new Store('Seatac Airport', 3, 24, 1.2);
+var sea = new Store('Seattle Center', 11, 38, 3.7);
+var capitol = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
 
-var pike = new store('1st and Pike', 23, 65, 6.3);
-var seaTac = new store('Seatac Airport', 3, 24, 1.2);
-var sea = new store('Seattle Center', 11, 38, 3.7);
-var capitol = new store('Capitol Hill', 20, 38, 2.3);
-var alki = new store('Alki', 2, 16, 4.6);
-pike.cookieSale();
-pike.show();
+
+pike.showInfo();
+
+//pike.show();
 // seaTac.cookieSale();
 // seaTac.show();
 // sea.cookieSale();
