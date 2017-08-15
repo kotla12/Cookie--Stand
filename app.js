ed3eceae44db8416ddd915f3dbebb89747e3d64b
kotla12 <kotla9119@gmail.com>
2017-08-15 04:48:28 -0700

    added dom to all stores

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-var openHours = ['6:00 a.m','7:00 a.m','8:00 a.m','9:00 a.m','10:00 a.m','11:00 a.m','12:00 p.m','1:00 p.m','2:00 p.m','3:00 p.m','4:00 p.m','5:00 p.m','6:00 p.m','7:00 p.m','8:00 p.m',];
+var openHours = ['6 a.m','7 a.m','8 a.m','9 a.m','10 a.m','11 a.m','12 p.m','1 p.m','2 p.m','3 p.m','4 p.m','5 p.m','6 p.m','7 p.m','8s p.m',];
 var pike = {
   name : '1st and Pike',
   minCust : 23,
@@ -11,7 +11,7 @@ var pike = {
   cookieSale : function () {
     for ( var i = 0; i < openHours.length; i++){
       // this calculates the average cookies sold per hour.
-      var sale = (Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie;
+      var sale = Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie);
       //console.log(sale);
       // here I'm pushing the results into aveCookieHourly array!
       this.aveCookieHourly.push(sale);
@@ -21,10 +21,22 @@ var pike = {
 
     console.log(this.totalCookies);
 
+  },
+  show: function() {
+    for (var i = 0; i < pike.aveCookieHourly.length; i++) {
+      var node = document.createElement('li');
+      var textnode = document.createTextNode(openHours[i] + ': ' + this.aveCookieHourly[i] + ' Cookies');
+      node.appendChild(textnode);
+      document.getElementById('pike').appendChild(node);
+    }
+    var dailyTotal = document.createElement('li');
+    dailyTotal.textContent = 'Total: ' + this.totalCookies + ' Cookies';
+    document.getElementById('pike').appendChild(dailyTotal);
   }
 
 };
 pike.cookieSale();
+pike.show();
 
 var seatac = {
   name : 'Seatac Airport',
@@ -37,7 +49,7 @@ var seatac = {
   cookieSale : function () {
     for ( var i = 0; i < openHours.length; i++){
       // this calculates the average cookies sold per hour.
-      var sale = (Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie;
+      var sale = Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie);
       //console.log(sale);
       // here I'm pushing the results into aveCookieHourly array!
       this.aveCookieHourly.push(sale);
@@ -47,10 +59,22 @@ var seatac = {
 
     console.log(this.totalCookies);
 
+  },
+  show: function() {
+    for (var i = 0; i < seatac.aveCookieHourly.length; i++) {
+      var node = document.createElement('li');
+      var textnode = document.createTextNode(openHours[i] + ': ' + this.aveCookieHourly[i] + ' Cookies');
+      node.appendChild(textnode);
+      document.getElementById('seatac').appendChild(node);
+    }
+    var dailyTotal = document.createElement('li');
+    dailyTotal.textContent = 'Total: ' + this.totalCookies + ' Cookies';
+    document.getElementById('seatac').appendChild(dailyTotal);
   }
 
 };
 seatac.cookieSale();
+seatac.show();
 
 var seattle = {
   name : 'Seattle Center',
@@ -63,7 +87,7 @@ var seattle = {
   cookieSale : function () {
     for ( var i = 0; i < openHours.length; i++){
       // this calculates the average cookies sold per hour.
-      var sale = (Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie;
+      var sale = Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie);
       //console.log(sale);
       // here I'm pushing the results into aveCookieHourly array!
       this.aveCookieHourly.push(sale);
@@ -73,10 +97,23 @@ var seattle = {
 
     console.log(this.totalCookies);
 
+  },
+  show: function() {
+    for (var i = 0; i < seattle.aveCookieHourly.length; i++) {
+      var node = document.createElement('li');
+      var textnode = document.createTextNode(openHours[i] + ': ' + this.aveCookieHourly[i] + ' Cookies');
+      node.appendChild(textnode);
+      document.getElementById('seattle').appendChild(node);
+    }
+    var dailyTotal = document.createElement('li');
+    dailyTotal.textContent = 'Total: ' + this.totalCookies + ' Cookies';
+    document.getElementById('seattle').appendChild(dailyTotal);
   }
 
 };
 seattle.cookieSale();
+seattle.show();
+
 var capitol = {
   name : 'Capitol Hill',
   minCust : 20,
@@ -88,7 +125,7 @@ var capitol = {
   cookieSale : function () {
     for ( var i = 0; i < openHours.length; i++){
       // this calculates the average cookies sold per hour.
-      var sale = (Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie;
+      var sale = Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie);
       //console.log(sale);
       // here I'm pushing the results into aveCookieHourly array!
       this.aveCookieHourly.push(sale);
@@ -98,10 +135,24 @@ var capitol = {
 
     console.log(this.totalCookies);
 
+  },
+  show: function() {
+    for (var i = 0; i < capitol.aveCookieHourly.length; i++) {
+      var node = document.createElement('li');
+      var textnode = document.createTextNode(openHours[i] + ': ' + this.aveCookieHourly[i] + ' Cookies');
+      node.appendChild(textnode);
+      document.getElementById('capitol').appendChild(node);
+    }
+    var dailyTotal = document.createElement('li');
+    dailyTotal.textContent = 'Total: ' + this.totalCookies + ' Cookies';
+    document.getElementById('capitol').appendChild(dailyTotal);
   }
 
 };
 capitol.cookieSale();
+capitol.show();
+
+
 var alki = {
   name : 'Alki',
   minCust : 2,
@@ -113,7 +164,7 @@ var alki = {
   cookieSale : function () {
     for ( var i = 0; i < openHours.length; i++){
       // this calculates the average cookies sold per hour.
-      var sale = (Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie;
+      var sale = Math.round((Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust)) * this.aveCookie);
       //console.log(sale);
       // here I'm pushing the results into aveCookieHourly array!
       this.aveCookieHourly.push(sale);
@@ -123,7 +174,19 @@ var alki = {
 
     console.log(this.totalCookies);
 
+  },
+  show: function() {
+    for (var i = 0; i < alki.aveCookieHourly.length; i++) {
+      var node = document.createElement('li');
+      var textnode = document.createTextNode(openHours[i] + ': ' + this.aveCookieHourly[i] + ' Cookies');
+      node.appendChild(textnode);
+      document.getElementById('alki').appendChild(node);
+    }
+    var dailyTotal = document.createElement('li');
+    dailyTotal.textContent = 'Total: ' + this.totalCookies + ' Cookies';
+    document.getElementById('alki').appendChild(dailyTotal);
   }
 
 };
 alki.cookieSale();
+alki.show();
