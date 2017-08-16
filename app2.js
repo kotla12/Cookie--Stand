c36925bc15b7be5fea40c5a05e8c16f380f50afb
kotla12 <kotla9119@gmail.com>
2017-08-15 22:06:16 -0700

    made working table data, last row for totals not complete

--- app2.js
+++ app2.js
@@ -19,14 +19,26 @@ function Store (name,minCust, maxCust, aveCookie ) {
     };
 
   };
+  // it creates tr for storeName/cookiesSoldPerHour/dailyCookies
   this.showInfo = function(){
     this.cookieSale ();
-
+//this creates 2nd row starting with pike
     var row = document.createElement('tr');
-    var storeName = document.createElement('th');
-    storeName.textContent = this.name;
+    var storeName = document.createElement('td');
+    storeName.innerHTML = this.name;
     row.appendChild(storeName);
-
+//this loop makes td for aveCookieHourly and puts them in a var cookieNums
+    for(var i = 0; i < openHours.length; i++) {
+      var cookiesNums = document.createElement('td');
+      cookiesNums.innerHTML = this.aveCookieHourly[i];
+      row.appendChild(cookiesNums);
+      tbl.appendChild(row);
+    }
+    // attaches last tr after hours td.
+    var dailyCookies = document.createElement('tr');
+    dailyCookies.innerHTML = this.totalCookies;
+    row.appendChild(dailyCookies);
+    tbl.appendChild(row);
 
   };
 }
@@ -37,8 +49,39 @@ var sea = new Store('Seattle Center', 11, 38, 3.7);
 var capitol = new Store('Capitol Hill', 20, 38, 2.3);
 var alki = new Store('Alki', 2, 16, 4.6);
 
+// creating the main table
+var tbl = document.createElement('table');
+var headerRow = document.createElement('thead');
+var space = document.createElement('td');
+headerRow.appendChild(space);
 
+
+// gets all opertaion hours and puts them between empty td and last tr (dailyTotal)
+for (var i = 0; i < openHours.length; i++) {
+  var tableData = document.createElement('th');
+  tableData.innerHTML = openHours[i];
+  headerRow.appendChild(tableData);
+};
+// adds last th of daily total to the first tr
+var dailyTotal = document.createElement('th');
+dailyTotal.innerHTML = 'Daily Location Total';
+headerRow.appendChild(dailyTotal);
+tbl.appendChild(headerRow);
+// adds last row in table starting with total!
+var footerRow = document.createElement('tfoot');
+var bottomTotal = document.createElement('th');
+bottomTotal.innerHTML = 'Totals';
+footerRow.appendChild(bottomTotal);
+tbl.appendChild(footerRow);
+//appends this table to div with id #table
+document.getElementById('table').appendChild(tbl);
+
+// call each store function
 pike.showInfo();
+seaTac.showInfo();
+sea.showInfo();
+capitol.showInfo();
+alki.showInfo();
 
 //pike.show();
 // seaTac.cookieSale();
